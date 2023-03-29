d32d19a9f0fee09179315f2fcc95365909f534a0
PehasJutt <apple@Apples-MacBook-Pro.local>
2023-03-29 05:47:23 +0500

    Dynamic Metatags Handling

--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react'
 import Blog from '../../comps/blogs/Blog' 
+import {MetaObjects} from '../../comps/helpers/Data'
+import MetaDecorator from '../../comps/MetaDecorator/MetaDecorator'
 
 class Blogs extends Component
 {
     render()
     {
+
+        let MetaObject = MetaObjects.filter((obj)=> obj.For === 'Blogs')[0]
+
         return(
 
             <div className = '_body'>
+                <MetaDecorator obj={MetaObject}/>
                 <Blog />
             </div>
             
--- src/pages/Doctors/Doctors.js
+++ src/pages/Doctors/Doctors.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react'
 import Doctor from '../../comps/doctors/Doctor'
+import {MetaObjects} from '../../comps/helpers/Data'
+import MetaDecorator from '../../comps/MetaDecorator/MetaDecorator'
 
 class Doctors extends Component
 {
     render()
     {
+        let MetaObject = MetaObjects.filter((obj)=> obj.For === 'Doctors')[0]
+
         return(
 
             <div className = '_body'>
+                <MetaDecorator obj={MetaObject}/>
                 <Doctor />
             </div>
             
