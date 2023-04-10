ebc56d98dfacffb999bc7d95c79bab7a8f138439
Ata Ul Mohsin <57590526+aumohsin99@users.noreply.github.com>
2023-04-10 09:57:33 +0500

    React form fields added

--- src/comps/popup/popup.js
+++ src/comps/popup/popup.js
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from "react";
-import { Form } from "react-final-form";
+import { Form, Field } from "react-final-form";
 import "./popup.css"; // import CSS file with blur-effect class
+
 
 const PopUp = ({ isDoctorForm, onClose, onSubmit }) => {
   const popUpRef = useRef(null);
@@ -19,22 +20,109 @@ const PopUp = ({ isDoctorForm, onClose, onSubmit }) => {
     };
   }, [onClose]);
 
+  const handleFormSubmit = (values) => {
+    onSubmit(values);
+    onClose();
+  };
+
   return (
     <div className="popup-container">
       <div className="popup" ref={popUpRef}>
-        <h2>{isDoctorForm ? "Doctor Form" : "Rescue Form"}</h2>
-        <Form
-          onSubmit={onSubmit}
-          render={({ handleSubmit }) => (
+        <h2>{isDoctorForm ? "Doctor Form" : "SOS Form"}</h2>
+        <Form onSubmit={handleFormSubmit}>
+          {({ handleSubmit }) => (
             <form onSubmit={handleSubmit}>
-              {/* Form fields */}
+              <Field name="name">
+                {({ input }) => (
+                  <div>
+                    <label>Name:</label>
+                    <input {...input} type="text" required />
+                  </div>
+                )}
+              </Field>
+              <Field name="address">
+                {({ input }) => (
+                  <div>
+                    <label>Address:</label>
+                    <input {...input} type="text" required />
+                  </div>
+                )}
+              </Field>
+              <Field name="phone">
+                {({ input }) => (
+                  <div>
+                    <label>Phone:</label>
+                    <input {...input} type="text" required />
+                  </div>
+                )}
+              </Field>
+              {isDoctorForm ? (
+                <div>
+                  <label>Doctor:</label>
+                  <Field name="doctor" component="select" required>
+                    <option value="">Select a doctor</option>
+                    <option value="Dr. Smith ($100)">Dr. Smith ($100)</option>
+                    <option value="Dr. Johnson ($150)">
+                      Dr. Johnson ($150)
+                    </option>
+                    <option value="Dr. Patel ($200)">Dr. Patel ($200)</option>
+                  </Field>
+                </div>
+              ) : (
+                <div>
+                  <label>Description:</label>
+                  <Field name="description">
+                    {({ input }) => (
+                      <textarea {...input} rows={5} required />
+                    )}
+                  </Field>
+                </div>
+              )}
               <button type="submit">Submit</button>
             </form>
           )}
-        />
+        </Form>
       </div>
     </div>
   );
 };
 
 export default PopUp;
+
+
+// const PopUp = ({ isDoctorForm, onClose, onSubmit }) => {
+//   const popUpRef = useRef(null);
+
+//   useEffect(() => {
+//     const handleMouseDown = (event) => {
+//       if (popUpRef.current && !popUpRef.current.contains(event.target)) {
+//         onClose();
+//       }
+//     };
+//     document.body.classList.add("blur-effect"); // add blur-effect class to body element
+//     document.addEventListener("mousedown", handleMouseDown);
+//     return () => {
+//       document.body.classList.remove("blur-effect"); // remove blur-effect class from body element
+//       document.removeEventListener("mousedown", handleMouseDown);
+//     };
+//   }, [onClose]);
+
+//   return (
+//     <div className="popup-container">
+//       <div className="popup" ref={popUpRef}>
+//         <h2>{isDoctorForm ? "Doctor Form" : "Rescue Form"}</h2>
+//         <Form
+//           onSubmit={onSubmit}
+//           render={({ handleSubmit }) => (
+//             <form onSubmit={handleSubmit}>
+//               {/* Form fields */}
+//               <button type="submit">Submit</button>
+//             </form>
+//           )}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default PopUp;
